98ecc212a67308e7448f82148f0e868523079750
Laust Johan Deleuran <ljd@ljd.dk>
2018-06-22 11:41:45 -0700

    Fix webpack dev task

--- webpack/dev.babel.js
+++ webpack/dev.babel.js
@@ -37,11 +37,56 @@ compiler.watch = true;
 
 // Setup dev server
 compiler.devServer = {
-	contentBase: paths.dest,
 	hot: true,
 	inline: true,
-	port: host.port,
-	public: host.name + ':' + host.port
+	// clientLogLevel: "none",
+	contentBase: paths.dest,
+	port: 3100,
+	disableHostCheck: true,
+	stats: {
+		// Add asset Information
+		assets: false,
+		// Sort assets by a field
+		assetsSort: 'field',
+		// Add information about cached (not built) modules
+		cached: false,
+		// Add children information
+		children: false,
+		// Add chunk information (setting this to `false` allows for a less verbose output)
+		chunks: false,
+		// Add built modules information to chunk information
+		chunkModules: false,
+		// Add the origins of chunks and chunk merging info
+		chunkOrigins: false,
+		// Sort the chunks by a field
+		chunksSort: 'field',
+		// Context directory for request shortening
+		context: paths.src,
+		// `webpack --colors` equivalent
+		colors: true,
+		// Add errors
+		errors: true,
+		// Add details to errors (like resolving log)
+		errorDetails: true,
+		// Add the hash of the compilation
+		hash: false,
+		// Add built modules information
+		modules: false,
+		// Sort the modules by a field
+		modulesSort: 'field',
+		// Add public path information
+		publicPath: false,
+		// Add information about the reasons why modules are included
+		reasons: false,
+		// Add the source code of modules
+		source: false,
+		// Add timing information
+		timings: true,
+		// Add webpack version information
+		version: false,
+		// Add warnings
+		warnings: true
+	}
 };
 
 
@@ -72,11 +117,11 @@ compiler.plugins = [
 	new BrowserSyncPlugin(
 		{
 			// browse to http://localhost:3100/ during development
-			host: config.domain,
+			host: 'localhost',
 			port: 3000,
 			// proxy the Webpack Dev Server endpoint
 			// through BrowserSync
-			proxy: 'http://' + config.domain + ':3100',
+			proxy: 'http://localhost:3100',
 			// Don't minify the client-side JS
 			minify: false
 		},
