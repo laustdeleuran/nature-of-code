824b3124fb1f0681d2aef5c8aac5df1dc3474fd0
Laust Deleuran <ljd@ljd.dk>
2019-04-30 18:34:19 -0700

    Now with shine

--- src/experiments/comic-buttons/button.js
+++ src/experiments/comic-buttons/button.js
@@ -1,4 +1,4 @@
-import rough from 'roughjs/dist/rough.umd';
+import log from 'koalition-utils/log';
 
 /**
  * SVG path helpers
@@ -29,11 +29,20 @@ const createSvgShadowPathLeft = (x, y, width, height) =>
  * @return {string} - svg path
  */
 const createSvgShadowPathRight = (x, y, width, height) =>
-	`M${x},${height - 7}L${x + width - 6},${y}c3.4,0,21.2,18.1,21.2,28.2
+	`M${x},${y + height - 7}L${x + width - 6},${y}c3.4,0,21.2,18.1,21.2,28.2
 		l0,${height - 25}c0,3.4-3.8,11.2-7.2,11.2
-		H${x + 30}C${x + 17.6},${y + height + 15},${x},${y + height - 4},${x},${y +
-		height -
-		7}z`;
+		H${x + 30}C${x + 17.6},${y + height + 15},
+		${x},${y + height - 4},${x},${y + height - 7}z`;
+
+// x = -100; y = -50
+// M-100,42.9L93.9-50c3.4,0,21.2,18.1,21.2,28.2
+//   v75.6c0,3.4-3.8,11.2-7.2,11.2H-69.7
+//   C-82.4,65-99.9,46.2-100,42.9z
+
+// x = 0; y = 0
+// M0,92.9L193.9,0c3.4,0,21.2,18.1,21.2,28.2
+//   v75.6c0,3.4-3.8,11.2-7.2,11.2H30.3
+//   C17.6,115,0.1,96.2,0,92.9z
 
 /**
  * Create button rect
@@ -53,8 +62,32 @@ const createSvgRectPath = (x, y, width, height) =>
 		3.76},${x + width - 2.76},${y + height},${x + width - 6.16},${y + height}Z`;
 
 /**
+ * Canvas helpers
+ */
+const lineToAngle = (context, x1, y1, length, angle) => {
+	angle *= Math.PI / 180;
+
+	const x2 = x1 + length * Math.cos(angle);
+	const y2 = y1 + length * Math.sin(angle);
+
+	context.beginPath();
+	context.moveTo(x1, y1);
+	context.lineTo(x2, y2);
+
+	return { x: x2, y: y2 };
+};
+
+/**
  * Button
  */
+const BUTTON_STATE_HIDDEN = 'BUTTON_STATE_HIDDEN';
+const BUTTON_STATE_APPEAR = 'BUTTON_STATE_APPEAR';
+const BUTTON_STATE_IDLE = 'BUTTON_STATE_IDLE';
+const BUTTON_STATE_SELECT = 'BUTTON_STATE_SELECT';
+const BUTTON_STATE_SELECT_DISAPPEAR = 'BUTTON_STATE_SELECT_DISAPPEAR';
+const BUTTON_STATE_UNSELECT = 'BUTTON_STATE_UNSELECT';
+const BUTTON_STATE_UNSELECT_DISAPPEAR = 'BUTTON_STATE_UNSELECT_DISAPPEAR';
+
 export default class Button {
 	/**
 	 * @constructor
@@ -74,7 +107,6 @@ export default class Button {
 	}) {
 		const canvas = document.createElement('canvas');
 		const context = canvas.getContext('2d');
-		const rc = rough.canvas(canvas);
 
 		element.appendChild(canvas);
 
@@ -85,9 +117,11 @@ export default class Button {
 			context,
 			element,
 			margin,
-			rc,
 			shadow,
+			step: 0,
 		};
+
+		this._state = BUTTON_STATE_HIDDEN;
 
 		this.resize();
 	}
@@ -96,44 +130,168 @@ export default class Button {
 	 * Step by step drawing function for animation
 	 */
 	draw() {
-		const { colorFace, colorShadow, margin, shadow, rc } = this._vars;
-		const { height, width } = this._size;
-		const shadowMethod =
-			shadow === 'right' ? createSvgShadowPathRight : createSvgShadowPathLeft;
+		const { width, height } = this._size;
+		if (!width || !height) {
+			log.warn('Button.draw() - Invalid size detected, draw disabled');
+			return;
+		}
 
-		const innerWidth = width - margin * 2;
-		const innerHeight = height - margin * 2;
+		const {
+			colorFace,
+			colorShadow,
+			context,
+			facePath,
+			shadowPath,
+			step,
+		} = this._vars;
+
+		if (!facePath || !shadowPath) {
+			log.warn('Button.draw() - No paths available, draw disabled');
+			return;
+		}
+
+		// Clear canvas
+		// context.clearRect(0, 0, width, height);
 
 		// Draw shadow
-		rc.path(shadowMethod(margin, margin, innerWidth, innerHeight), {
-			roughness: 0.25,
-			fill: colorShadow,
-			fillStyle: 'solid',
-			stroke: colorShadow,
-		});
+		context.fillStyle = colorShadow;
+		context.fill(shadowPath);
 
 		// Draw button rect
-		rc.path(createSvgRectPath(margin, margin, innerWidth, innerHeight), {
-			roughness: 0.25,
-			fill: colorFace,
-			fillStyle: 'solid',
-			stroke: colorFace,
-		});
+		context.fillStyle = colorFace;
+		context.fill(facePath);
+
+		// Draw shine
+		// this.drawShine();
+
+		// Count frames for animation purposes
+		this._vars.step = step + 1;
+	}
+
+	/**
+	 * Draw shine
+	 */
+	drawShine() {
+		const { width, height } = this._size;
+
+		const { context, facePath, margin, shadow, step } = this._vars;
+
+		const fps = 60;
+		const transition = 0.67; // seconds;
+		const wait = 2; // seconds
+		const seconds = (step / fps) % (transition + wait);
+
+		if (seconds < transition) {
+			const pct = seconds / transition;
+			const innerWidth = width - margin * 2;
+			const angle = 80 * (shadow === 'right' ? 1 : -1);
+			const y = shadow === 'right' ? 0 : height;
+			// const innerHeight = height - margin * 2;
+
+			context.save();
+
+			context.clip(facePath);
+			context.globalCompositeOperation = 'screen';
+
+			let x = width * 1.5 * pct - width * 0.25;
+			context.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+
+			context.lineWidth = Math.round(innerWidth * 0.2);
+			lineToAngle(context, x, y, height, angle);
+			context.stroke();
+
+			context.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+			context.lineWidth = Math.round(innerWidth * 0.05);
+			lineToAngle(context, x + innerWidth * 0.175, y, height, angle);
+			context.stroke();
+
+			context.restore();
+		}
 	}
 
 	/**
 	 * Resize button based on container size.
 	 */
 	resize() {
-		const { canvas, element } = this._vars;
+		const { canvas, context, element } = this._vars;
 		const { width, height } = element.getBoundingClientRect();
 
 		this._size = {
 			width,
 			height,
+			x: width * -0.5,
+			y: height * -0.5,
 		};
 
 		canvas.width = width;
 		canvas.height = height;
+
+		context.setTransform(1, 0, 0, 1, 0, 0); // Reset transformation matrix
+		context.translate(width * 0.5, height * 0.5); // Set 0-0 to center of canvas
+
+		this.createPaths();
+	}
+
+	/**
+	 * Create paths
+	 */
+	createPaths() {
+		const { margin, shadow } = this._vars;
+		const { height, width, x, y } = this._size;
+
+		if (!width || !height) {
+			log.warn(
+				'Button.createPaths() - Invalid size detected, couldnt create paths'
+			);
+			return;
+		}
+
+		const shadowMethod =
+			shadow === 'right' ? createSvgShadowPathRight : createSvgShadowPathLeft;
+
+		const innerWidth = width - margin * 2;
+		const innerHeight = height - margin * 2;
+
+		this._vars.shadowPath = new Path2D(
+			shadowMethod(x + margin, y + margin, innerWidth, innerHeight)
+		);
+		this._vars.facePath = new Path2D(
+			createSvgRectPath(x + margin, y + margin, innerWidth, innerHeight)
+		);
+	}
+
+	/**
+	 * Set state
+	 * @param {enum} state
+	 */
+	setState(state) {
+		this._state = state;
+
+		clearTimeout(this._vars.stateTimer);
+		if (state === BUTTON_STATE_APPEAR)
+			this._vars.stateTimer = setTimeout(
+				() => this.setState(BUTTON_STATE_IDLE),
+				333
+			);
+		// else if (state === BUTTON_STATE_SELECT)
+		// 	this._vars.stateTimer = setTimeout(
+		// 		() => this.setState(BUTTON_STATE_SELECT_DISAPPEAR),
+		// 		333
+		// 	);
+		else if (state === BUTTON_STATE_UNSELECT)
+			this._vars.stateTimer = setTimeout(
+				() => this.setState(BUTTON_STATE_UNSELECT_DISAPPEAR),
+				250
+			);
+		else if (state === BUTTON_STATE_SELECT_DISAPPEAR)
+			this._vars.stateTimer = setTimeout(
+				() => this.setState(BUTTON_STATE_HIDDEN),
+				500
+			);
+		else if (state === BUTTON_STATE_UNSELECT_DISAPPEAR)
+			this._vars.stateTimer = setTimeout(
+				() => this.setState(BUTTON_STATE_HIDDEN),
+				250
+			);
 	}
 }
